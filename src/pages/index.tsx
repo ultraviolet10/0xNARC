5355ecaa134f5e1c4f85a31a85b75226bb10a70f
ultraviolet10 <aritrac1998@hotmail.com>
2023-12-09 00:05:36 +0530

    chore: button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,6 @@ export default function Home() {
     // api call to server to make openapi request
     // feed in docs, zipped
     // need blob?
-
     // const data = await fetch("/api/validate")
     // console.log({ data })
   }, [])
@@ -31,15 +30,22 @@ export default function Home() {
         <div className="flex flex-col space-y-4 p-10 items-center justify-center">
           <span className="font-white text-[25px]">0xNARC</span>
 
-          <input
-            className={
-              isValidUrl
-                ? "flex h-[50px] w-[500px] rounded-xl shadow-light-900 p-4 text-black"
-                : "flex h-[50px] w-[500px] rounded-xl shadow-light-900 p-4 text-black border-[1px] border-red-400"
-            }
-            onChange={(e) => setGitUrl(e.target.value)}
-            onClick={handleVerify}
-          ></input>
+          <div className="flex flex-col grow items-center justify-center space-y-10">
+            <input
+              className={
+                isValidUrl
+                  ? "flex h-[50px] w-[500px] rounded-xl shadow-light-900 p-4 text-black"
+                  : "flex h-[50px] w-[500px] rounded-xl shadow-light-900 p-4 text-black border-[1px] border-red-400"
+              }
+              onChange={(e) => setGitUrl(e.target.value)}
+            ></input>
+            <button
+              className="flex w-[165px] bg-[#35274E] flex-row items-center justify-center rounded-xl py-2 text-center text-base font-medium text-white shadow-sm hover:bg-[#9C4FFF] hover:text-white"
+              onClick={handleVerify}
+            >
+              Verify
+            </button>
+          </div>
         </div>
       </div>
     </main>
