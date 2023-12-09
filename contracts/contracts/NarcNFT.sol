b0a05c03d772592b7a9fb139e8b4e43bee611a03
prnth <pranith_h@yahoo.com.sg>
2023-12-10 01:53:19 +0530

    this solidity compiles

--- contracts/contracts/NarcNFT.sol
+++ contracts/contracts/NarcNFT.sol
@@ -1,41 +1,53 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721 {
+contract MyToken is ERC721 {
     using Counters for Counters.Counter;
-    Counters.Counter private currentTokenId;
+    Counters.Counter private _tokenIdCounter;
 
-    // Mapping from token ID to the timestamp of last transfer
-    mapping(uint256 => uint256) private lastTransferTimestamp;
+    // Time lock duration (3 months in seconds)
+    uint256 public constant lockDuration = 3 * 30 days;
 
-    constructor() ERC721("NFT Name", "NFT") {}
+    // Mapping from token ID to the timestamp when it was minted
+    mapping(uint256 => uint256) private mintTimestamps;
 
-    function mint(address recipient)
-        public
-        returns (uint256)
-    {
-        currentTokenId.increment();
-        uint256 tokenId = currentTokenId.current();
-        _safeMint(recipient, tokenId);
-        // Set the initial transfer time as the minting time
-        lastTransferTimestamp[tokenId] = block.timestamp;
-        return tokenId;
+    constructor() ERC721("OxNARC Badge", "0xNARC") {}
+
+    // Override the transferFrom function
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        require(
+            block.timestamp >= mintTimestamps[tokenId] + lockDuration,
+            "Token is locked and cannot be transferred yet"
+        );
+        super.transferFrom(from, to, tokenId);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-        // Check if the token was transferred in the last 3 months
+    // Override the safeTransferFrom function
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public override {
         require(
-            block.timestamp - lastTransferTimestamp[tokenId] >= 3 * 30 days,
-            "Transfer not allowed yet"
+            block.timestamp >= mintTimestamps[tokenId] + lockDuration,
+            "Token is locked and cannot be transferred yet"
         );
-        // Update the last transfer timestamp
-        lastTransferTimestamp[tokenId] = block.timestamp;
+        super.safeTransferFrom(from, to, tokenId, _data);
+    }
+
+    // Public function to mint tokens
+    function safeMint(address to) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _mint(to, tokenId);
+        mintTimestamps[tokenId] = block.timestamp;
     }
 }
